87a441d01ef3de52e47514bc15904b6ee21c06f2
lipeng759581712 <759581712@qq.com>
2017-07-21 19:36:25 +0800

    增加Date方法和Time方法

--- Utils/src/main/java/com/max/utils/TimeUtils.java
+++ Utils/src/main/java/com/max/utils/TimeUtils.java
@@ -276,4 +276,393 @@ public class TimeUtils
 
 		return df.format(new Date(timeSec));
 	}
+
+	/**
+	 * 秒  转换成  xx:xx:xx  时：分：秒格式
+	 * @param time
+	 * @return
+	 */
+	public static String secToTime(int time) {
+		String timeStr = null;
+		int hour = 0;
+		int minute = 0;
+		int second = 0;
+		if (time <= 0)
+			return "00:00";
+		else {
+			minute = time / 60;
+			if (minute < 60) {
+				second = time % 60;
+				timeStr = unitFormat(minute) + ":" + unitFormat(second);
+			} else {
+				hour = minute / 60;
+				if (hour > 99)
+					return "99:59:59";
+				minute = minute % 60;
+				second = time - hour * 3600 - minute * 60;
+				timeStr = unitFormat(hour) + ":" + unitFormat(minute) + ":" + unitFormat(second);
+			}
+		}
+		return timeStr;
+	}
+
+	private static String unitFormat(int i) {
+		String retStr = null;
+		if (i >= 0 && i < 10)
+			retStr = "0" + Integer.toString(i);
+		else
+			retStr = "" + i;
+		return retStr;
+	}
+
+	public static final String DEFAULT_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+	/**
+	 * 将时间戳转为时间字符串
+	 * <p>格式为yyyy-MM-dd HH:mm:ss</p>
+	 *
+	 * @param millis 毫秒时间戳
+	 * @return 时间字符串
+	 */
+	public static String millis2String(long millis) {
+		return new SimpleDateFormat(DEFAULT_PATTERN, Locale.getDefault()).format(new Date(millis));
+	}
+
+	/**
+	 * 将时间戳转为时间字符串
+	 * <p>格式为pattern</p>
+	 *
+	 * @param millis  毫秒时间戳
+	 * @param pattern 时间格式
+	 * @return 时间字符串
+	 */
+	public static String millis2String(long millis, String pattern) {
+		return new SimpleDateFormat(pattern, Locale.getDefault()).format(new Date(millis));
+	}
+
+
+
+
+
+
+
+
+	/**
+	 * 将Date类型转为时间字符串
+	 * <p>格式为yyyy-MM-dd HH:mm:ss</p>
+	 *
+	 * @param date Date类型时间
+	 * @return 时间字符串
+	 */
+	public static String date2String(Date date) {
+		return date2String(date, DEFAULT_PATTERN);
+	}
+
+	/**
+	 * 将Date类型转为时间字符串
+	 * <p>格式为pattern</p>
+	 *
+	 * @param date    Date类型时间
+	 * @param pattern 时间格式
+	 * @return 时间字符串
+	 */
+	public static String date2String(Date date, String pattern) {
+		return new SimpleDateFormat(pattern, Locale.getDefault()).format(date);
+	}
+
+	/**
+	 * 将Date类型转为时间戳
+	 *
+	 * @param date Date类型时间
+	 * @return 毫秒时间戳
+	 */
+	public static long date2Millis(Date date) {
+		return date.getTime();
+	}
+
+	/**
+	 * 将时间戳转为Date类型
+	 *
+	 * @param millis 毫秒时间戳
+	 * @return Date类型时间
+	 */
+	public static Date millis2Date(long millis) {
+		return new Date(millis);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * 获取当前毫秒时间戳
+	 *
+	 * @return 毫秒时间戳
+	 */
+	public static long getNowTimeMills() {
+		return System.currentTimeMillis();
+	}
+
+	/**
+	 * 获取当前时间字符串
+	 * <p>格式为yyyy-MM-dd HH:mm:ss</p>
+	 *
+	 * @return 时间字符串
+	 */
+	public static String getNowTimeString() {
+		return millis2String(System.currentTimeMillis(), DEFAULT_PATTERN);
+	}
+
+	/**
+	 * 获取当前时间字符串
+	 * <p>格式为pattern</p>
+	 *
+	 * @param pattern 时间格式
+	 * @return 时间字符串
+	 */
+	public static String getNowTimeString(String pattern) {
+		return millis2String(System.currentTimeMillis(), pattern);
+	}
+
+	/**
+	 * 获取当前Date
+	 *
+	 * @return Date类型时间
+	 */
+	public static Date getNowTimeDate() {
+		return new Date();
+	}
+
+
+
+
+
+
+	/**
+	 * 判断是否闰年
+	 *
+	 * @param date Date类型时间
+	 * @return {@code true}: 闰年<br>{@code false}: 平年
+	 */
+	public static boolean isLeapYear(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		int year = cal.get(Calendar.YEAR);
+		return isLeapYear(year);
+	}
+
+	/**
+	 * 判断是否闰年
+	 *
+	 * @param millis 毫秒时间戳
+	 * @return {@code true}: 闰年<br>{@code false}: 平年
+	 */
+	public static boolean isLeapYear(long millis) {
+		return isLeapYear(millis2Date(millis));
+	}
+
+	/**
+	 * 判断是否闰年
+	 *
+	 * @param year 年份
+	 * @return {@code true}: 闰年<br>{@code false}: 平年
+	 */
+	public static boolean isLeapYear(int year) {
+		return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+	}
+
+
+
+
+	/**
+	 * 获取星期
+	 *
+	 * @param date Date类型时间
+	 * @return 星期
+	 */
+	public static String getWeek(Date date) {
+		return new SimpleDateFormat("EEEE", Locale.getDefault()).format(date);
+	}
+
+	/**
+	 * 获取星期
+	 *
+	 * @param millis 毫秒时间戳
+	 * @return 星期
+	 */
+	public static String getWeek(long millis) {
+		return getWeek(new Date(millis));
+	}
+
+
+
+
+
+	/**
+	 * 获取星期
+	 * <p>注意：周日的Index才是1，周六为7</p>
+	 *
+	 * @param date Date类型时间
+	 * @return 1...7
+	 */
+	public static int getWeekIndex(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		return cal.get(Calendar.DAY_OF_WEEK);
+	}
+
+	/**
+	 * 获取星期
+	 * <p>注意：周日的Index才是1，周六为7</p>
+	 *
+	 * @param millis 毫秒时间戳
+	 * @return 1...7
+	 */
+	public static int getWeekIndex(long millis) {
+		return getWeekIndex(millis2Date(millis));
+	}
+
+
+	/**
+	 * 获取月份中的第几周
+	 * <p>注意：国外周日才是新的一周的开始</p>
+	 *
+	 * @param date Date类型时间
+	 * @return 1...5
+	 */
+	public static int getWeekOfMonth(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		return cal.get(Calendar.WEEK_OF_MONTH);
+	}
+
+	/**
+	 * 获取月份中的第几周
+	 * <p>注意：国外周日才是新的一周的开始</p>
+	 *
+	 * @param millis 毫秒时间戳
+	 * @return 1...5
+	 */
+	public static int getWeekOfMonth(long millis) {
+		return getWeekOfMonth(millis2Date(millis));
+	}
+
+
+
+
+	/**
+	 * 获取年份中的第几周
+	 * <p>注意：国外周日才是新的一周的开始</p>
+	 *
+	 * @param date Date类型时间
+	 * @return 1...54
+	 */
+	public static int getWeekOfYear(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		return cal.get(Calendar.WEEK_OF_YEAR);
+	}
+
+	/**
+	 * 获取年份中的第几周
+	 * <p>注意：国外周日才是新的一周的开始</p>
+	 *
+	 * @param millis 毫秒时间戳
+	 * @return 1...54
+	 */
+	public static int getWeekOfYear(long millis) {
+		return getWeekOfYear(millis2Date(millis));
+	}
+
+	private static final String[] CHINESE_ZODIAC = {"猴", "鸡", "狗", "猪", "鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊"};
+
+
+
+
+	/**
+	 * 获取生肖
+	 *
+	 * @param date Date类型时间
+	 * @return 生肖
+	 */
+	public static String getChineseZodiac(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		return CHINESE_ZODIAC[cal.get(Calendar.YEAR) % 12];
+	}
+
+	/**
+	 * 获取生肖
+	 *
+	 * @param millis 毫秒时间戳
+	 * @return 生肖
+	 */
+	public static String getChineseZodiac(long millis) {
+		return getChineseZodiac(millis2Date(millis));
+	}
+
+	/**
+	 * 获取生肖
+	 *
+	 * @param year 年
+	 * @return 生肖
+	 */
+	public static String getChineseZodiac(int year) {
+		return CHINESE_ZODIAC[year % 12];
+	}
+
+	private static final String[] ZODIAC       = {"水瓶座", "双鱼座", "白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "魔羯座"};
+	private static final int[]    ZODIAC_FLAGS = {20, 19, 21, 21, 21, 22, 23, 23, 23, 24, 23, 22};
+
+
+
+
+
+	/**
+	 * 获取星座
+	 *
+	 * @param date Date类型时间
+	 * @return 星座
+	 */
+	public static String getZodiac(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		int month = cal.get(Calendar.MONTH) + 1;
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		return getZodiac(month, day);
+	}
+
+	/**
+	 * 获取星座
+	 *
+	 * @param millis 毫秒时间戳
+	 * @return 星座
+	 */
+	public static String getZodiac(long millis) {
+		return getZodiac(millis2Date(millis));
+	}
+
+	/**
+	 * 获取星座
+	 *
+	 * @param month 月
+	 * @param day   日
+	 * @return 星座
+	 */
+	public static String getZodiac(int month, int day) {
+		return ZODIAC[day >= ZODIAC_FLAGS[month - 1]
+				? month - 1
+				: (month + 10) % 12];
+	}
+
+
 }
